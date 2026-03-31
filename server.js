89d69810650919bd4df3890e90da2c9b42d2da43
Wanjala Joseph Poya <njaalajoseph.com>
2026-03-31 08:56:39 +0300

    Add download routes with /my endpoint for user downloads

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ const newsletterRoutes   = require('./routes/newsletterRoutes');
 const bookingRoutes      = require('./routes/bookingRoutes');
 const blogRoutes         = require('./routes/blogRoutes');
 const adminRoutes        = require('./routes/adminRoutes');
+const downloadRoutes = require('./routes/downloadRoutes');
 
 const app = express();
 app.set('trust proxy', 1);
